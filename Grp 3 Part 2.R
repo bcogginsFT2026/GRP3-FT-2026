72de96e92d3cae86edec42d62f1bc52f4539e56b
BradC <kxhy6xf8r7@privaterelay.appleid.com>
2026-02-17 12:08:04 -0500

    Added 2 plots

--- Grp 3 Part 2.R
+++ Grp 3 Part 2.R
@@ -3,6 +3,7 @@
 library(tidyr)
 library(dplyr)
 library(readr)
+library(ggplot2)
 
 insurance <- read_csv("insurance.csv")
 
@@ -10,8 +11,37 @@ str(insurance)
 spec(insurance)
 spec_csv("insurance.csv")
 
+# Display the column names and data types.
 insurance |>
 sapply(insurance, function(x) n_distinct(x)) |>
 summarise(insurance,(count = n()))
+
+# Display a count of unique values for each column.
+summarise(across(everything(), list(min = min, max = max)))
+distinct(insurance, across(everything()))
+unique(insurance) 
+count(unique(insurance))
+
+insurance |> apply(MARGIN = 2, FUN = unique) |>
+       str()
+
+insurance |> apply(MARGIN = 2, FUN = unique) |>
+  sapply(FUN = length) |>  
+  str()
+
+sapply(insurance, unique)
+insurance |>
+summarise(across(everything(), ~ n_distinct(.)))
+
+# Generate Plot of Charges distributed | USE Density Plot
+#How are the BMI values distributed? Are there any outliers?   
+insurance |>
+ggplot(data = insurance, mapping = aes(x = bmi, y = charges)) +
+         geom_density_2d()
+   
+#What’s the relationship between age and charges?
+insurance |>
+  ggplot(data = insurance, mapping = aes(x = age, y = charges)) +
+  geom_density_2d()
 
 
